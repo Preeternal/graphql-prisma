cc9e88facf1424e0a8e8e8c980532ea9e6757b87
Preeternal <preeternal@ya.ru>
2019-05-08 08:52:43 +0300

    Init commit

---
+++ src/resolvers/User.js
@@ -0,0 +1,35 @@
+import getUserId from "../utils/getUserId";
+const User = {
+  posts: {
+    fragment: "fragment userId on User { id }",
+    resolve(parent, args, { prisma }, info) {
+      return prisma.query.posts({
+        where: {
+          published: true,
+          author: {
+            id: parent.id
+          }
+        }
+      });
+    }
+  },
+  email: {
+    fragment: "fragment userId on User { id }",
+    resolve(parent, args, { request }, info) {
+      const userId = getUserId(request, false);
+      if (userId && userId === parent.id) {
+        return parent.email;
+      } else {
+        return null;
+      }
+    }
+  },
+  password: {
+    fragment: "fragment userId on User { id }",
+    resolve(parent, args, {}, info) {
+      return "secret";
+    }
+  }
+};
+
+export { User as default };
