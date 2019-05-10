f996394964ba6c4462553d2bfe8bc4eeae08f65a
Preeternal <preeternal@ya.ru>
2019-05-10 20:15:51 +0300

    use beforeEach with jest

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,48 @@
 import "cross-fetch/polyfill";
 import ApolloBoost, { gql } from "apollo-boost";
+import bcrypt from "bcryptjs";
 import prisma from "../src/prisma";
 
-jest.setTimeout(30000);
+// jest.setTimeout(30000);
 
 const client = new ApolloBoost({
   uri: "http://localhost:4000"
+});
+
+beforeEach(async () => {
+  await prisma.mutation.deleteManyPosts();
+  await prisma.mutation.deleteManyUsers();
+  const user = await prisma.mutation.createUser({
+    data: {
+      name: "Jen",
+      email: "jen@mail.com",
+      password: bcrypt.hashSync("dark#@123dd")
+    }
+  });
+  await prisma.mutation.createPost({
+    data: {
+      title: "some post",
+      body: "",
+      published: true,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  });
+  await prisma.mutation.createPost({
+    data: {
+      title: "some post2",
+      body: "",
+      published: false,
+      author: {
+        connect: {
+          email: "jen@mail.com"
+        }
+      }
+    }
+  });
 });
 
 test("Should create a new user", async () => {
