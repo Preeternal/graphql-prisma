ba4017f26702f5e518eff41d2c7c659532c2052d
Preeternal <preeternal@ya.ru>
2019-05-12 17:43:35 +0300

    set variables for post.test

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,9 +1,8 @@
 import "cross-fetch/polyfill";
-import { gql } from "apollo-boost";
 import prisma from "../src/prisma";
 import seedDatabase, { userOne, postOne, postTwo } from "./utils/seedDatabase";
 import getClient from "./utils/getClient";
-import { selectHttpOptionsAndBody } from "apollo-link-http-common";
+import { getPosts, myPosts, updatePost, createPost, deletePost } from "./utils/operations";
 
 jest.setTimeout(30000);
 
@@ -12,22 +11,6 @@ const client = getClient();
 beforeEach(seedDatabase);
 
 test("Should expose published posts", async () => {
-  const getPosts = gql`
-    query {
-      posts {
-        id
-        title
-        body
-        published
-        author {
-          name
-        }
-        comments {
-          text
-        }
-      }
-    }
-  `;
   const response = await client.query({ query: getPosts });
   expect(response.data.posts.length).toBe(1);
   expect(response.data.posts[0].published).toBe(true);
@@ -35,38 +18,19 @@ test("Should expose published posts", async () => {
 
 test("Should fetch myPosts", async () => {
   const client = getClient(userOne.jwt);
-  const myPosts = gql`
-    query {
-      myPosts {
-        id
-        title
-        body
-        published
-      }
-    }
-  `;
   const { data } = await client.query({ query: myPosts });
   expect(data.myPosts.length).toBe(2);
 });
 
 test("Should be able to update own post", async () => {
   const client = getClient(userOne.jwt);
-  const updatePost = gql`
-    mutation {
-      updatePost(
-        id: "${postOne.post.id}",
-        data: {
-          published: false
-        }
-      ) {
-        id
-        title
-        body
-        published
-      }
+  const variables = {
+    id: postOne.post.id,
+    data: {
+      published: false
     }
-  `;
-  const { data } = await client.mutate({ mutation: updatePost });
+  };
+  const { data } = await client.mutate({ mutation: updatePost, variables });
   const exists = await prisma.exists.Post({ id: postOne.post.id, published: false });
   expect(data.updatePost.published).toBe(false);
   expect(exists).toBe(true);
@@ -74,17 +38,10 @@ test("Should be able to update own post", async () => {
 
 test("Should create a new post", async () => {
   const client = getClient(userOne.jwt);
-  const createPost = gql`
-    mutation {
-      createPost(data: { title: "Post #3", body: "", published: true }) {
-        id
-        title
-        body
-        published
-      }
-    }
-  `;
-  const { data } = await client.mutate({ mutation: createPost });
+  const variables = {
+    data: { title: "Post #3", body: "", published: true }
+  };
+  const { data } = await client.mutate({ mutation: createPost, variables });
   const posts = await prisma.query.posts();
   expect(data.createPost.title).toBe("Post #3");
   expect(posts.length).toBe(3);
@@ -92,19 +49,10 @@ test("Should create a new post", async () => {
 
 test("Should delete post", async () => {
   const client = getClient(userOne.jwt);
-  const deletePost = gql`
-  mutation {
-    deletePost(
-      id: "${postTwo.post.id}"
-    ){
-      id
-      title
-      body
-      published
-    }
-  }
-  `;
-  const { data } = await client.mutate({ mutation: deletePost });
+  const variables = {
+    id: postTwo.post.id
+  };
+  const { data } = await client.mutate({ mutation: deletePost, variables });
   const posts = await prisma.query.posts();
   const exists = await prisma.exists.Post({ id: postTwo.post.id });
   expect(data.deletePost.id).toBe(postTwo.post.id);
--- tests/utils/operations.js
+++ tests/utils/operations.js
@@ -42,4 +42,75 @@ const getProfile = gql`
   }
 `;
 
-export { createUser, login, getUsers, getProfile };
+const getPosts = gql`
+  query {
+    posts {
+      id
+      title
+      body
+      published
+      author {
+        name
+      }
+      comments {
+        text
+      }
+    }
+  }
+`;
+
+const myPosts = gql`
+  query {
+    myPosts {
+      id
+      title
+      body
+      published
+    }
+  }
+`;
+
+const updatePost = gql`
+  mutation($id: ID!, $data: UpdatePostInput!) {
+    updatePost(id: $id, data: $data) {
+      id
+      title
+      body
+      published
+    }
+  }
+`;
+
+const createPost = gql`
+  mutation($data: CreatePostInput!) {
+    createPost(data: $data) {
+      id
+      title
+      body
+      published
+    }
+  }
+`;
+
+const deletePost = gql`
+  mutation($id: ID!) {
+    deletePost(id: $id) {
+      id
+      title
+      body
+      published
+    }
+  }
+`;
+
+export {
+  createUser,
+  login,
+  getUsers,
+  getProfile,
+  getPosts,
+  myPosts,
+  updatePost,
+  createPost,
+  deletePost
+};
